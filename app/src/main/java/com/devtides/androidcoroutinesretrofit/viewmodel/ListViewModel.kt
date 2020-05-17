0a5d052bc5a339cd18917ef15829d2421bed6ab1
Vinoth <vinobaskar03@gmail.com>
2020-05-17 07:15:39 -0700

    Master Code Uploded
    Coroutine Retrofit2 master uploded

---
+++ app/src/main/java/com/devtides/androidcoroutinesretrofit/viewmodel/ListViewModel.kt
@@ -0,0 +1,50 @@
+package com.devtides.androidcoroutinesretrofit.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.devtides.androidcoroutinesretrofit.model.CountriesService
+import com.devtides.androidcoroutinesretrofit.model.Country
+import kotlinx.coroutines.*
+
+class ListViewModel : ViewModel() {
+    private val countriesApi = CountriesService.getCountriesService()
+    private var job: Job? = null
+    private val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        onError("Exception : ${throwable.localizedMessage}")
+    }
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<String?>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        loading.value = true
+
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            val response = countriesApi.getCountires()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    countries.value = response.body()
+                    countryLoadError.value = null
+                    loading.value = false
+                } else {
+                    onError("Error : ${response.message()}")
+                }
+            }
+        }
+    }
+
+    private fun onError(message: String) {
+        countryLoadError.value = message
+        loading.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
+    }
+
+}
